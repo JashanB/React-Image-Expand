3da4ff065dc5c6b6add45f7b950e33decceb302e
JashanB <jashan_brar50@hotmail.com>
2020-11-08 19:30:15 -0500

    adding project structure

---
+++ src/components/app/App.js
@@ -0,0 +1,60 @@
+// import React, { useState } from 'react';
+import React from 'react';
+import './App.css';
+import Project from '../project';
+import Profile from '../profile';
+import WorkExp from '../workExp';
+import Publications from '../publications';
+
+export default function App() {
+  // const [collapse, setCollapse] = useState(false); 
+  // const collapseMenu = () => {
+  //   collapse ? setCollapse(state => false) : setCollapse(state => true);
+  // };
+
+  return (
+    <div className="App">
+      {/* <button onClick={() => collapseMenu()}></button> */}
+      <div className="nav">
+        <h2>Jashan Brar</h2>
+        <ul className="section-list">
+          <li>
+            <a href="#profile">Profile</a>
+          </li>
+          <li>
+            <a href="#work-exp">Work Experience</a>
+          </li>
+          <li>
+            <a href="#project">Projects</a>
+          </li>
+            <ul className="project-list">
+              <li>
+                <a href="#Bettr-Trip.">Bettr Trip.</a>
+              </li>
+              <li>
+                <a href="#WeatherApp">WeatherApp</a>
+              </li>
+              <li>
+                <a href="#Newton">Newton</a>
+              </li>
+              <li>
+                <a href="#Life">Game of Life</a>
+              </li>
+              <li>
+                <a href="#Number-Guesser">Number Guesser</a>
+              </li>
+            </ul>
+            <li>
+              <a href="#publications">Publications</a>
+            </li>
+        </ul>
+      </div>
+        <div className="content-wrapper">
+          <Profile />
+          <WorkExp />
+          <Project />
+          <Publications />
+        </div>
+      </div>
+      );
+    }
---
+++ src/components/profile/Profile.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import './Profile.css';
+
+export default function Profile(props) {
+  return (
+    <div id="profile">
+      <div className="image-container">
+        <img alt="profile-img" src="crop1.png"></img>
+      </div>
+      <div className="picture-border"></div>
+      <h1>Jashan Brar</h1>
+      <p className="info-p"><b>Lighthouse Labs</b> 2019-2020</p>
+      <p className="info-p"><b>Schulich School of Medicine</b> 2016-2018</p>
+      <p className="info-p"><b>Western University</b> 2015, Biomedical Cellular Biology</p>
+      <div className="contact-flex">
+        <p className="contact-1"><b>contact:</b> brarjashan417@gmail.com</p><p> </p>
+        <p className="contact-1"><b>github</b>: <a href="https://github.com/JashanB">https://github.com/JashanB</a></p><p> </p>
+        <p className="contact-2"><b>linkedin</b>: <a href="https://www.linkedin.com/in/jashan-brar/">https://www.linkedin.com/in/jashan-brar/</a></p>
+      </div>
+      <div className="about">
+        <h2>About</h2>
+        <p>I am a previous medical student that decided to pursue my passion for technology and development.
+          For me, medicine and programming are very similar. Both careers can be described as a craft with an infinite skill ceiling.
+          To truly excel, you must be able to continuously learn new information, and adapt to changes in the field.
+          As someone whose basic qualities are curiousity and drive, this process is extremely engaging and motivates me to try my hardest.</p>
+        <p>In the short amount of time that I have learnt development, I have created a few things that I am proud of.
+          I made a travel planner that filled a hole in my planning needs, a weather app that provides me with more information than the one on my phone,
+           and a Pinterest-clone for educational resources. I know this is only the start and I am keen to develop my skills.</p>
+        <p>I am excited to continue to create and I hope you follow me on my creative journey. </p>
+      </div>
+      <h2>Skills</h2>
+      <div className="skills-flex">
+        <div className="skill-item">
+          <h4>Languages</h4>
+          <ul>
+            <li>
+              <p>JavaScript</p>
+            </li>
+            <li>
+              <p>Ruby</p>
+            </li>
+            <li>
+              <p>HTML</p>
+            </li>
+            <li>
+              <p>Python</p>
+            </li>
+            <li>
+              <p>CSS</p>
+            </li>
+            <li>
+              <p>SASS</p>
+            </li>
+          </ul>
+        </div>
+        <div className="skill-item">
+          <h4>Frameworks, Libraries & Environments</h4>
+          <ul>
+            <li>
+              <p>React</p>
+            </li>
+            <li>
+              <p>Express</p>
+            </li>
+            <li>
+              <p>NodeJS</p>
+            </li>
+            <li>
+              <p>Rails</p>
+            </li>
+            <li>
+              <p>JQuery</p>
+            </li>
+            <li>
+              <p>Bootstrap</p>
+            </li>
+          </ul>
+        </div>
+        <div className="skill-item">
+          <h4>Systems, CMS & Databases</h4>
+          <ul>
+            <li>
+              <p>Git</p>
+            </li>
+            <li>
+              <p>PostgreSQL</p>
+            </li>
+            <li>
+              <p>SQL</p>
+            </li>
+            <li>
+              <p>MongoDB</p>
+            </li>
+            <li>
+              <p>GraphQL</p>
+            </li>
+            <li>
+              <p>Google Cloud</p>
+            </li>
+          </ul>
+        </div>
+        <div className="skill-item">
+          <h4>Testing</h4>
+          <ul>
+            <li>
+              <p>Cypress</p>
+            </li>
+            <li>
+              <p>Jest</p>
+            </li>
+            <li>
+              <p>Mocha/Chai</p>
+            </li>
+            <li>
+              <p>React Testing Library</p>
+            </li>
+            <li>
+              <p>Enzyme</p>
+            </li>
+            <li>
+              <p>Apollo</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div >
+  )
+}
