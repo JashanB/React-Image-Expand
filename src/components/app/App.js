9ef1bdefc6481312967ecd06c8b9b435bed556ff
JashanB <jashan_brar50@hotmail.com>
2020-11-08 19:33:05 -0500

    setup structure

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,7 @@
 // import React, { useState } from 'react';
 import React from 'react';
 import './App.css';
-import Project from '../project';
-import Profile from '../profile';
-import WorkExp from '../workExp';
-import Publications from '../publications';
+import Modal from '../modal';
 
 export default function App() {
   // const [collapse, setCollapse] = useState(false); 
@@ -14,47 +11,7 @@ export default function App() {
 
   return (
     <div className="App">
-      {/* <button onClick={() => collapseMenu()}></button> */}
-      <div className="nav">
-        <h2>Jashan Brar</h2>
-        <ul className="section-list">
-          <li>
-            <a href="#profile">Profile</a>
-          </li>
-          <li>
-            <a href="#work-exp">Work Experience</a>
-          </li>
-          <li>
-            <a href="#project">Projects</a>
-          </li>
-            <ul className="project-list">
-              <li>
-                <a href="#Bettr-Trip.">Bettr Trip.</a>
-              </li>
-              <li>
-                <a href="#WeatherApp">WeatherApp</a>
-              </li>
-              <li>
-                <a href="#Newton">Newton</a>
-              </li>
-              <li>
-                <a href="#Life">Game of Life</a>
-              </li>
-              <li>
-                <a href="#Number-Guesser">Number Guesser</a>
-              </li>
-            </ul>
-            <li>
-              <a href="#publications">Publications</a>
-            </li>
-        </ul>
-      </div>
-        <div className="content-wrapper">
-          <Profile />
-          <WorkExp />
-          <Project />
-          <Publications />
-        </div>
-      </div>
-      );
-    }
+
+    </div>
+  );
+}
