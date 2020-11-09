58d85a5eaaabcc77929db74e72295f1ed71a41a7
JashanB <jashan_brar50@hotmail.com>
2020-11-08 21:34:28 -0500

    reluctantly works

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,49 +1,32 @@
 // import React, { useState } from 'react';
 import React from 'react';
 import './App.css';
-import Modal from '../modal';
+import beach from './previews/beach-preview.jpg'
+import tree from './previews/tree-preview.jpg'
+import mountain from './previews/mountain-preview.jpg'
+import forest from './previews/forest-preview.jpg'
 
 export default function App() {
-  const imageObjectArr = [{
-     id: 1,
-     alt: 'Beach',
-     dataOriginal: 'beach-preview.jpg',
-     src: '../../previews/beach-preview.jpg'
-  },
-  {
-    id: 2,
-     alt: 'Forest',
-     dataOriginal: 'forest-preview.jpg',
-     src: '../../previews/forest-preview.jpg'
-  },
-  {
-    id: 3,
-     alt: 'Mountain',
-     dataOriginal: 'mountain-preview.jpg',
-     src: '../../previews/mountain-preview.jpg'
-  },
-  {
-    id: 4,
-     alt: 'tree',
-     dataOriginal: 'tree-preview.jpg',
-     src: '../../previews/tree-preview.jpg'
-  }
-];
-  const imageList = imageObjectArr.map(function(image) {
-    return (
-      <Modal 
-        key={image.id}
-        onClick={}
-        alt={image.alt}
-        dataOriginal={image.dataOriginal}
-        src={image.src}
-        
-      />
-    )
-  });
+  
   return (
-    <div className="App">
-      {imageList}
+    <div className="gallery">
+      <div className="img-container">
+        <img src={beach} alt="Beach" data-original="beach-preview.jpg" />
+
+      </div>
+      <div className="img-container">
+        <img src={forest} alt="Forest" data-original="forest-preview.jpg" />
+
+      </div>
+      <div className="img-container">
+        <img src={mountain} alt="Mountain" data-original="mountain-preview.jpg" />
+
+      </div>
+      <div className="img-container">
+        <img src={tree} alt="tree" data-original="tree-preview.jpg" />
+
+      </div>
+
     </div>
   );
 }
