1667321334e6c91fc4784fc3384cbdfa3e68fb18
JashanB <jashan_brar50@hotmail.com>
2020-11-08 19:46:14 -0500

    adding modal list

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,14 +4,46 @@ import './App.css';
 import Modal from '../modal';
 
 export default function App() {
-  // const [collapse, setCollapse] = useState(false); 
-  // const collapseMenu = () => {
-  //   collapse ? setCollapse(state => false) : setCollapse(state => true);
-  // };
-
+  const imageObjectArr = [{
+     id: 1,
+     alt: 'Beach',
+     dataOriginal: 'beach-preview.jpg',
+     src: '../../previews/beach-preview.jpg'
+  },
+  {
+    id: 2,
+     alt: 'Forest',
+     dataOriginal: 'forest-preview.jpg',
+     src: '../../previews/forest-preview.jpg'
+  },
+  {
+    id: 3,
+     alt: 'Mountain',
+     dataOriginal: 'mountain-preview.jpg',
+     src: '../../previews/mountain-preview.jpg'
+  },
+  {
+    id: 4,
+     alt: 'tree',
+     dataOriginal: 'tree-preview.jpg',
+     src: '../../previews/tree-preview.jpg'
+  }
+];
+  const imageList = imageObjectArr.map(function(image) {
+    return (
+      <Modal 
+        key={image.id}
+        onClick={}
+        alt={image.alt}
+        dataOriginal={image.dataOriginal}
+        src={image.src}
+        
+      />
+    )
+  });
   return (
     <div className="App">
-
+      {imageList}
     </div>
   );
 }
--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -3,6 +3,8 @@ import './Modal.css';
 
 export default function Modal(props) {
   return (
-   
+    <div className="img-container">
+      <img src="./previews/beach-preview.jpg" alt="Beach" data-original="beach-preview.jpg" />
+    </div>
   )
 }
